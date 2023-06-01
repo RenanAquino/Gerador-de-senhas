babd3c267ff48d244ef01a857948c1cd941a870e
Renan Aquino <126978540+RenanAquino@users.noreply.github.com>
2023-06-01 20:24:06 -0300

    Update gerador de senhas.py

--- gerador de senhas.py
+++ gerador de senhas.py
@@ -6,4 +6,4 @@ car = int(input('\033[1;32mQuantos caracteres você quer nessa senha? \033[m'))
 senha="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345678910@#+-[]*_;?"
 print('\033[33mSua senha é:\033[m',end=' ')
 for c in range(0,car):
-	print(random.choice(senha),end=' ')
+	print(random.choice(senha),end='')
