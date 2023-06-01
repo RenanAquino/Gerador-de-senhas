9c4debaf5307b611f652935304f4df3b93e1f966
Renan Aquino <126978540+RenanAquino@users.noreply.github.com>
2023-06-01 20:22:33 -0300

    Add files via upload

---
+++ gerador de senhas.py
@@ -0,0 +1,9 @@
+import random
+print('\033[7;31m \033[m'*44)
+print('            GERADOR DE SENHAS')
+print('\033[7;31m \033[m'*44)
+car = int(input('\033[1;32mQuantos caracteres você quer nessa senha? \033[m'))
+senha="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345678910@#+-[]*_;?"
+print('\033[33mSua senha é:\033[m',end=' ')
+for c in range(0,car):
+	print(random.choice(senha),end=' ')
